4d0a9b0f9f822312b2e85ab71dc05931d9cf39bf
Erin Hancock <welljer@gmail.com>
2019-01-18 18:46:44 -0700

    forth push

---
+++ assets/javascript/game.js
@@ -0,0 +1,91 @@
+"use strict"
+$(document).ready(function() {
+
+    var possibleWords = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+
+    const maxGuess = 10
+    var pauseGame = false
+
+    var guessedLetters = []
+    var guessingWord = []
+    var wordToMatch
+    var numGuess
+    var wins = 0
+
+    resetGame()
+
+    document.onkeypress = function(event) {
+        if (isAlpha(event.key) && !pauseGame) {
+            checkForLetter(event.key.toUpperCase())
+        }
+    }
+
+    function checkForLetter(letter) {
+        var foundLetter = false
+        var correctSound = document.createElement("audio")
+        var incorrectSound = document.createElement("audio")
+        correctSound.setAttribute("src", "assets/sounds/stairs.mp3")
+        incorrectSound.setAttribute("src","assets/sounds/croak.mp3")
+
+        for (var i=0, j= wordToMatch.length; i<j; i++) {
+            if (letter === wordToMatch[i]) {
+                guessingWord[i] = letter
+                foundLetter = true
+                correctSound.play()
+                if (guessingWord.join("") === wordToMatch) {
+                    wins++
+                    pauseGame = true
+                    updateDisplay()
+                    setTimeout(resetGame,5000)
+                }
+            }
+        }
+
+        if (!foundLetter) {
+            incorrectSound.play()
+            if (!guessedLetters.includes(letter)) {
+                guessedLetters.push(letter)
+                numGuess--
+            }
+            if (numGuess === 0) {
+                guessingWord = wordToMatch.split()
+                pauseGame = true
+                setTimeout(resetGame, 200)
+            }
+        }
+
+        updateDisplay()
+
+    }
+    function isAlpha (ch){
+        return /^[A-Z]$/i.test(ch);
+    }
+
+    function resetGame() {
+        numGuess = maxGuess
+        pauseGame = false
+
+        wordToMatch = possibleWords[Math.floor(Math.random() * possibleWords.length)].toUpperCase()
+        console.log(wordToMatch)
+
+        guessedLetters = []
+        guessingWord = []
+
+        for (var i=0, j=wordToMatch.length; i < j; i++){
+            if (wordToMatch[i] === " ") {
+                guessingWord.push(" ")
+            } else {
+                guessingWord.push("_")
+            }
+        }
+
+        updateDisplay()
+    }
+
+    function updateDisplay () {
+        document.getElementById("totalWins").innerText = wins
+        document.getElementById("currentWord").innerText = guessingWord.join("")
+        document.getElementById("remainingGuesses").innerText = numGuess
+        document.getElementById("guessedLetters").innerText =  guessedLetters.join(" ")
+    }
+})
