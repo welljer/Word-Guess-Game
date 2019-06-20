80ee0e92e2f402b129f6108336dc1594d924c39e
Jeremy Cantwell <welljer@gmail.com>
2019-06-19 19:41:37 -0600

    slight updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,31 @@
 "use strict"
 $(document).ready(function() {
+// Setting up and declaring variables
+    let possibleWords = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 
-    var possibleWords = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
-
-    const maxGuess = 10
-    var pauseGame = false
-
-    var guessedLetters = []
-    var guessingWord = []
-    var wordToMatch
-    var numGuess
-    var wins = 0
+    const maxGuess = 10;
+    let guessedLetters= [];
+    let guessingWord= [];
+    let wordToMatch;
+    let numGuess;
+    let wins= 0;
+    let pauseGame= false
 
     resetGame()
 
-    document.onkeypress = function(event) {
+    document.onkeypress = event => {
         if (isAlpha(event.key) && !pauseGame) {
             checkForLetter(event.key.toUpperCase())
         }
     }
 
     function checkForLetter(letter) {
-        var foundLetter = false
-        var correctSound = document.createElement("audio")
-        var incorrectSound = document.createElement("audio")
-        correctSound.setAttribute("src", "assets/sounds/stairs.mp3")
-        incorrectSound.setAttribute("src","assets/sounds/croak.mp3")
+        let foundLetter = false
 
-        for (var i=0, j= wordToMatch.length; i<j; i++) {
+        for (let i=0, j= wordToMatch.length; i<j; i++) {
             if (letter === wordToMatch[i]) {
                 guessingWord[i] = letter
                 foundLetter = true
-                correctSound.play()
                 if (guessingWord.join("") === wordToMatch) {
                     wins++
                     pauseGame = true
@@ -42,7 +36,6 @@ $(document).ready(function() {
         }
 
         if (!foundLetter) {
-            incorrectSound.play()
             if (!guessedLetters.includes(letter)) {
                 guessedLetters.push(letter)
                 numGuess--
